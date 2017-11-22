3c42959306d10a115a155d258e18ef98a053b810
rmillett <rmillett@student.cccd.edu>
2017-11-21 16:17:54 -0800

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/caffeinefinder/CaffeineDetailsActivity.java
@@ -0,0 +1,20 @@
+package edu.orangecoastcollege.cs273.caffeinefinder;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ryan Millett on 11/21/2017.
+ */
+class CaffeineDetailsActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caffeine_details);
+
+
+        
+    }
+}
